281a42750da961f224cf6115e2d4bfbebd185c83
aurkaxi <abdullahalmuaz15@gmail.com>
2024-02-08 18:05:38 +0600

    Altered 112A
    https://codeforces.com/contest/112/submission/245459616

--- src/a112.rs
+++ src/a112.rs
@@ -2,10 +2,8 @@
 // https://codeforces.com/problemset/problem/112/A
 
 /*
-* We will iter through each nth charcter for both and compare them.
-* If they are not equal then we don't need to compare the rest of the string. We will just print the result.
-* If they are equal then we continue to the next character untill we find a difference or reach the end of the string.
-* If we reach the end of the string then the strings are equal.
+* Umm, well in rust we can just use the cmp method to compare strings.
+* It compares them lexicographically by default.
 */
 
 pub fn main() {
@@ -16,24 +14,36 @@ pub fn main() {
     let str1 = str1.trim().to_lowercase();
     let str2 = str2.trim().to_lowercase();
 
-    let len = str1.len();
+    // let len = str1.len();
 
-    for i in 0..len {
-        let current_char_from_str1 = str1.chars().nth(i).unwrap();
-        let current_char_from_str2 = str2.chars().nth(i).unwrap();
-        match current_char_from_str1.cmp(&current_char_from_str2) {
-            std::cmp::Ordering::Less => {
-                println!("-1");
-                return;
-            }
-            std::cmp::Ordering::Greater => {
-                println!("1");
-                return;
-            }
-            std::cmp::Ordering::Equal => {
-                continue;
-            }
+    // for i in 0..len {
+    //     let current_char_from_str1 = str1.chars().nth(i).unwrap();
+    //     let current_char_from_str2 = str2.chars().nth(i).unwrap();
+    //     match current_char_from_str1.cmp(&current_char_from_str2) {
+    //         std::cmp::Ordering::Less => {
+    //             println!("-1");
+    //             return;
+    //         }
+    //         std::cmp::Ordering::Greater => {
+    //             println!("1");
+    //             return;
+    //         }
+    //         std::cmp::Ordering::Equal => {
+    //             continue;
+    //         }
+    //     }
+    // }
+    // println!("0");
+
+    match str1.cmp(&str2) {
+        std::cmp::Ordering::Less => {
+            println!("-1");
+        }
+        std::cmp::Ordering::Greater => {
+            println!("1");
+        }
+        std::cmp::Ordering::Equal => {
+            println!("0");
         }
     }
-    println!("0");
 }
